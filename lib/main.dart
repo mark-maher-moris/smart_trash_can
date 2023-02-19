fe0636397892398f1733cc9ff84e881634996222
AnasDarwesh20 <anasdarwesh2022@gmail.com.com>
2023-02-19 20:33:12 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'TrashIt',
       home: Text(
-        'hello trashit'
+        'hello test trashit'
       ) ,
     );
   }
