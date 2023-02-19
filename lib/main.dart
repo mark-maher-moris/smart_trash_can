1c94c1db5a6d8ea6199448cab9f07e1f51442d68
Mark <markmaher933@gmail.com>
2023-02-19 21:23:26 +0200

    Mark Commit 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TrashIt',
+      home: Text('1111111111111111111111111111111'),
+    );
+  }
+}
