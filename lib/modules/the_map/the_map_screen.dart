4da20860af2a1b8c60a953975c295b9300bd6057
AnasDarwesh20 <anasdarwesh2022@gmail.com.com>
2023-02-21 15:38:50 +0200

    this is the map screen

---
+++ lib/modules/the_map/the_map_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TheMapScreen extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return  Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
+
